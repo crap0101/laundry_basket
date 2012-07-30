4b3ea119621296dd2e6972f25fd6a91972af777a
Marco Chieppa <crap0101@riseup.net>
2012-07-30 07:01:06 +0200

    dircheck_hash.sh modified

--- dircheck_hash.sh
+++ dircheck_hash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# dircheck_hash v.0.7
-# find differences between two directories
+# dircheck_hash v.0.8
+# find differences between two directories by file names
 
 # Copyright (C) 2010  Marco Chieppa (aka crap0101)
 
@@ -28,19 +28,24 @@
 
 function usage ()
 {
-    echo -ne "dircheck_hash: compare files by names between dirs.
-    USAGE: $0 [-n] [-m MAXDEPTH] [-c HASHPROG] -f DIR1 [-s DIR2]
+    echo -ne "dircheck_hash: compare files by names between dirs by file names.
+    (version: $VERSION)
+    USAGE: $0 [-n] [-m maxdepth] [-c hash_prog] [-i] dir1 dir2
+    DESCRIPTION;
+        Each FILENAME in the first directory will be compared against FILENAME
+        in the second directory (if any).
+    POSITIONAL ARGUMENTS:
+        dir1     1st dir to compare
+        dir2     2nd dir to compare
     OPTIONS:
-        -h       print this help and exit
-        -v       print program's version and exit
-        -n       Do not count files
-        -m NUM   Descend at most NUM (a  non-negative  integer) levels  of
-                 directories. -maxdepth 0 means  only  apply  the  tests and
-                 actions to the command line arguments
         -c EXE   program to use for file comparison (default md5sum)
-
-        -f PATH  First directory to compare (default to the current dir)
-        -s PATH  Other directory to compare (default to the current dir)\n"
+        -h       print this help and exit
+        -i       swap directories comparison order (dir1 dir2 become dir2 dir1)
+        -m NUM   Descend at most NUM (a  non-negative  integer) levels  of
+                 directories. -m 0 means  only  apply  the  tests and
+                 actions to the command line arguments
+        -n       Do not count files
+        -v       print program's version and exit\n"
 }
 
 if [ $# -lt 1 ]; then
@@ -48,39 +53,35 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-_VERSION=0.7
+VERSION=0.8
+max_depth=32000
+must_count=1
+total="-"
+ext_err=0
+hash_prog=md5sum
+swap_dirs=0
+dir1=
+dir2=
 
-MAXDEPTH=32767
-IS_COUNT=1
-TOTAL="-"
-DIR1=$PWD
-DIR2=$PWD
-EXT_ERR=0
-HASHPROG=md5sum
-
-while getopts "hvnm:c:f:s:" arg
+while getopts "c:him:nv" opt
 do
-    case $arg in
-        c)  HASHPROG=$OPTARG
+    case $opt in
+        c)  hash_prog=$OPTARG
+            ;;
+        i)  swap_dirs=1
             ;;
         m)
-            MAXDEPTH=$OPTARG
-            if [ $MAXDEPTH -lt 0 ]; then
+            max_depth=$OPTARG
+            if [ $max_depth -lt 0 ]; then
                 usage
                 exit 255
             fi
             ;;
         n)
-            IS_COUNT=0
-            ;;
-        f)
-            DIR1=${OPTARG%/}
-            ;;
-        s)
-            DIR2=${OPTARG%/}
+            must_count=0
             ;;
         v)
-            echo $_VERSION
+            echo $VERSION
             exit 0
             ;;
         *|h)
@@ -89,56 +90,83 @@ do
     esac
 done
 
+shift $(($OPTIND - 1))
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
+fi
 
-if [ $IS_COUNT -eq 1 ]; then
-    TOTAL=`find "$DIR1" -maxdepth $MAXDEPTH -type f | wc -l`
+for i in 1st 2nd; do
+    if [ $# -gt 0 ]; then
+        if [ -z "$dir1" ]; then 
+            dir1="$1"
+        elif [ -z "$dir2" ]; then
+            dir2="$1"
+        fi
+        shift
+    fi
+done
+
+#if [[ $# -gt 0 && -n "$dir1" && -n "$dir2" ]]; then
+#    echo "Error: Unknown extra arguments ($#)"
+#    exit 1
+#fi
+if [ -z "$dir1" -o -z "$dir2" ]; then
+    echo "Error: not enough arguments!"
+    exit 1
+fi
+if [ $# -gt 0 ]; then
+    echo "Error: Unknown extra arguments ($#)"
+    exit 1
+fi
+
+if [ $swap_dirs -eq 1 ]; then
+    tmp="$dir1"
+    dir1="$dir2"
+    dir2="$tmp"
 fi
 
 
-function dcheck
+function dcheck ()
 {
-    errors=0
     count=0
+    errors=0
     skipped=0
-    DIR1=$1
-    DIR2=$2
-    MAXDEPTH=$3
-    TOTAL=$4
-    HASHPROG=$5
-    IFS=$'\n';
-    for file in `find "$DIR1" -maxdepth $MAXDEPTH -type f`
+    dir1="$1"
+    dir2="$2"
+    max_depth=$3
+    hash_prog=$4
+    if [ $5 -eq 1 ]; then
+        total=$(find "$dir1" -maxdepth $max_depth -type f | wc -l)
+    else
+        total="-"
+    fi
+    find "$dir1" -maxdepth $max_depth -type f | while read file
     do
         (( count++ ))
-        hashfile1=`$HASHPROG "$file" | awk '{print $1}'`
-        append=${file:${#DIR1}}
-        if [ ! -e "$DIR2/$append" ]
-        then
-	    echo -e "\n(not found, skipped) $DIR2/$append"
+        hashfile1=$($hash_prog "$file" | awk '{print $1}')
+        append=${file:${#dir1}}
+        if [ ! -e "$dir2/$append" ]; then
+	        echo -e "\nm: $dir2/$append"
             (( skipped++ ))
         else
-            hashfile2=`$HASHPROG "$DIR2/$append" | awk '{print $1}'`
-            if [ "$hashfile1" != "$hashfile2" ]
-            then
+            hashfile2=$($hash_prog "$dir2/$append" | awk '{print $1}')
+            if [ $hashfile1 != $hashfile2 ]; then
                 (( errors++ ))
-                echo -e "\nhash mismatch: $file\t$DIR2/$append"
+                echo -e "\nh: $file\t$dir2/$append"
             fi
         fi
-        _i="\rchecking file $count/$TOTAL\tErrors:$errors\tSkipped:$skipped"
+        _i="\ri: checking file $count/$total\tErrors:$errors\tSkipped:$skipped"
         echo -ne "$_i"
-
     done
-    if [ $skipped -ne 0 ]; then
-        EXT_ERR=1
-    fi
-    if [ $errors -ne 0 ]; then
-       EXT_ERR=2
-    fi
     echo
+    if [ $skipped -ne 0 -o $errors -ne 0 ]; then
+        return 1
+    else
+        return 0
+    fi
 }
 
-
-
-echo "checksum for files in $DIR1 against files in $DIR2"
-dcheck "$DIR1" "$DIR2" $MAXDEPTH $TOTAL $HASHPROG
-
-exit $EXT_ERR
+echo "i: checksum for files in $dir1 against files in $dir2"
+dcheck "$dir1" "$dir2" $max_depth $hash_prog $must_count
+exit $?
