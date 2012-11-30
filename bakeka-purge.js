667e4e6b4ecc0bd1f4b224a915b5230587639762
Marco Chieppa <crap0101@riseup.net>
2012-11-30 06:59:18 +0100

    fix error

--- bakeka-purge.js
+++ bakeka-purge.js
@@ -22,11 +22,11 @@
 // @run-at    document-end
 // ==/UserScript==
 
-var ShitReg = /Il nostro Cliente[:]?|Per ditta di servizi|per azienda|per\s(azienda(\scliente)?|cliente)|per\s(important[ei]|prestigios[iao]|primari[ao]?)\s(client[ei]|azienda|realtà)|ali spa|consorzio elpe|Gruppo Elpe|Viesse|Knet Human Resources|HUMANGEST SPA|TEMPORARY SPA|Obiettivo Lavoro|Adecco|randstad|manpower|kelly|Synergie|Viesse|Gi Group|Cooperjob|ManpowerGroup|Agenzia per il Lavoro/im;
+var ShitReg = /Il nostro Cliente[:]?|per nostro cliente|Per ditta di servizi|per azienda|per\s(azienda(\scliente)?|cliente)|per\s(important[ei]|prestigios[iao]|primari[ao]?)\s(client[ei]|azienda|realtà|negozio)|elektraservices|ali spa|consorzio elpe|Gruppo Elpe|Viesse|Knet Human Resources|HUMANGEST SPA|TEMPORARY SPA|Obiettivo Lavoro|Adecco|randstad|manpower|kelly|Synergie|Viesse|Elite Executive Research|Gi Group|Cooperjob|Start People|gruppo gedi|Orienta spa|ManpowerGroup|Agenzia per il Lavoro/im;
 
 var bakekapurge = function () {
     var divs = document.getElementsByTagName("div");
-    for (var i=0; i<divs.length; i++) {
+    for (var i=divs.length-1; i >= 0; i--) { //    for (var i=0; i<divs.length; i++) {
 	var div = divs[i];
 	if (div.className.indexOf("annuncio-item") == 0) {
 	    var elems = div.getElementsByTagName("p");
@@ -35,6 +35,7 @@ var bakekapurge = function () {
 		var el = elems[j];
 		if (el.className == "text" && ShitReg.exec(el.innerHTML)) {
 		    //console.log(el.innerHTML);
+		    //el.innerHTML = "[PURGED]";
 		    div.parentNode.removeChild(div);
 		    break;
 		}
