80d2cf823d4c19b91c8111e0b06b92ba325cb5d3
Marco Chieppa <c24p0101@gmail.com>
2025-05-28 01:30:19 +0200

    add some options to py_grep

--- py_grep.py
+++ py_grep.py
@@ -8,6 +8,48 @@ import sys
 MATCHING_FUNCS = ('match', 'search')
 PATTERN = 'PATTERN'
 
+class FormatPrint:
+    def __init__(self, use_filename=False, use_line_num=False):
+        self._use_filename = use_filename
+        self._use_line_num = use_line_num
+    def use_filename(self, bool_value):
+        self._use_filename = bool(bool_value)
+    def use_line_num(self, bool_value):
+        self._use_line_num = bool(bool_value)
+    def get_format(self):
+        fmt = '{line}'
+        if self._use_line_num:
+            fmt = '{line_num}:' + fmt
+        if self._use_filename:
+            fmt = '{filename}:' + fmt
+        return fmt
+
+def file_with_match(infile, matching_funcs, __unused_max_count, __unused_print_func):
+    with (open(infile) if infile != '-' else sys.stdin) as f:
+        for _ in matching_lines(f, matching_funcs):
+            print(infile)
+            break
+
+def file_without_match(infile, matching_funcs, __unused_max_count, __unused_print_func):
+    with (open(infile) if infile != '-' else sys.stdin) as f:
+        for _ in matching_lines(f, matching_funcs):
+            break
+        else:
+            print(infile)
+        
+def standard_search (infile, matching_funcs, max_count, format_print): #print_func):
+    with (open(infile) if infile != '-' else sys.stdin) as f:
+        _format = format_print.get_format()
+        for match_num, (line_num, m) in enumerate(matching_lines(f, matching_funcs), start=1):
+            print(_format.format(filename=infile, line_num=line_num, line=m), end='')
+            #print_func(line_num, m, '')
+            if match_num == max_count:
+                break
+
+def pattern_from_file (path):
+    with open(path) as f:
+        return list(line.strip() for line in f)
+
 def fixed_string_match (pattern):
     def match_line (line):
         return pattern in line
@@ -40,15 +82,30 @@ def get_parser():
     parser.add_argument('-F', '--fixed-strings',
                         dest='fixed_strings', action='store_true',
                         help=f'Interpret any {PATTERN} as fixed strings, not regular expressions.')
+    parser.add_argument('-f', '--file',
+                        dest='from_file', metavar='FILE',
+                        help='''Obtain patterns from FILE, one per line.
+                        Leading and trailing whitespaces are stripped from the patterns.''')
+    parser.add_argument('-H', '--with-filename',
+                        dest='with_filename', action='store_true',
+                        help='Print the file name for each match.')
     parser.add_argument('-i', '--ignore-case',
                         dest='re_flags', action=SetFlag, default=0, const=re.I,
                         help="Perform case-insensitive matching.")
-    # line-oriented, not used:
-    #parser.add_argument('-m', '--multiline',
-    #                    dest='re_flags', action=SetFlag, default=0, const=re.M)
+    parser.add_argument('-l', '--files-with-match',
+                        dest='files_with_match', action='store_true',
+                        help='''Suppress normal output; instead print the name of each
+                        input file from which output would normally have been printed.
+                        Scanning each input file stops upon first match.''')
+    parser.add_argument('-L', '--files-without-match',
+                        dest='files_without_match', action='store_true',
+                        help='''Suppress  normal  output; instead print the name of
+                        each input file from which no output would normally have been printed.''')
     parser.add_argument('-M', '--matching-function',
                         dest='matching_func', choices=MATCHING_FUNCS, default='search',
-                        help='''Regex matching function (python's re.match or re.search).''')
+                        help='''Regex matching function (python's re.match or re.search).
+                        See the python's documentation for the different
+                        behaviour of these functions.''')
     parser.add_argument('-m', '--max-count',
                         dest='max_count', type=int, default=-1, metavar='NUM',
                         help='''Stop reading a file after %(metavar)s matching lines.
@@ -62,7 +119,10 @@ def get_parser():
                         dest='invert', action='store_true',
                         help='''Invert the sense of matching, to select non-matching lines.
                         NOTE: works when there's one pattern only present at command line.''')
-    parser.add_argument('pattern', metavar=PATTERN, help='regex pattern to match')
+    parser.add_argument('pattern', metavar=PATTERN,
+                        help='''Default regex pattern to match.
+                        To use the patterns provided with the -e or -F options only,
+                        pass the empty string '' as the default pattern.''')
     parser.add_argument('files', nargs='*')
     return parser
 
@@ -71,14 +131,30 @@ if __name__ == '__main__':
     __errors = 0
     parser = get_parser()
     parsed = parser.parse_args()
-    if parsed.line_number:
-        __print = lambda n, l, e: print(f'{n}:{l}', end=e)
-    else:
-        __print = lambda n, l, e: print(l, end=e)
+    #conflict:
+    if parsed.max_count and (parsed.files_with_match or parsed.files_without_match):
+        print(f'{parser.prog}: WARNING: "--max-count" ignored when'
+              ' using "--files-with-match" or "--files-without-match"!',
+              file=sys.stderr)
+    # output formatting:
+    format_print = FormatPrint()
+    format_print.use_line_num(parsed.line_number)
+    format_print.use_filename(parsed.with_filename)
+    # input files:
     if not parsed.files:
         parsed.files.append('-')
+    # patterns to match:
     __patterns = parsed.extra_patterns
-    __patterns.append(parsed.pattern)
+    if parsed.pattern:
+        __patterns.append(parsed.pattern)
+    if parsed.from_file:
+        try:
+            __patterns.extend(pattern_from_file(parsed.from_file))
+        except OSError as e:
+            parser.error(e)
+    if not __patterns:
+        parser.error("No pattern specified!")
+    # string/regex matching functions:
     __matching_funcs = []
     if parsed.fixed_strings:
         for p in __patterns:
@@ -86,16 +162,20 @@ if __name__ == '__main__':
     else:
         for p in __patterns:
             __matching_funcs.append(getattr(re.compile(p, flags=parsed.re_flags), parsed.matching_func))
+    # ...
     if parsed.invert:
         __matching_funcs = [lambda arg: not f(arg) for f in __matching_funcs]
-    for file in parsed.files:
+    # do it:
+    for infile in parsed.files:
         try:
-            with (open(file) if file != '-' else sys.stdin) as f:
-                for match_num, (line_num, m) in enumerate(matching_lines(f, __matching_funcs), start=1):
-                    __print(line_num, m, '')
-                    if match_num == parsed.max_count:
-                        break
+            if parsed.files_with_match:
+                file_with_match(infile, __matching_funcs, 1, lambda i:i)
+            elif parsed.files_without_match:
+                file_without_match(infile, __matching_funcs, 1, lambda i:i)
+            else:
+                standard_search(infile, __matching_funcs, parsed.max_count, format_print)
         except (ValueError, PermissionError, FileNotFoundError) as e:
-            print(f"{parser.prog}: ERROR with file {file}: {e}", file=sys.stderr)
+            print(f"{parser.prog}: ERROR with file {infile}: {e}", file=sys.stderr)
             __errors += 1
     sys.exit(__errors)
+
